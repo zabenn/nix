eeb2ac0b08f7084ca8d697c88cbbde0b732bfa74
Sam Cribbs <samuel_cribbs@berkeley.edu>
2025-10-07 15:33:26 -0700

    Docker layer daily flush

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -5,12 +5,35 @@
   users.extraGroups.docker.members = [ "sam" ];
 
   hardware.nvidia-container-toolkit.enable = true;
+
   virtualisation.docker = {
     enable = true;
     liveRestore = false;
     daemon.settings = {
       features = {
         cdi = true;
+      };
+    };
+  };
+
+  systemd = {
+    services."docker-cleanup" = {
+      script = ''
+        ${pkgs.docker}/bin/docker system prune -f --filter "until=$((30*24))h"
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        After = [ "docker.service" ];
+        Requires = [ "docker.service" ];
+      };
+    };
+    timers."docker-cleanup" = {
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+        Unit = "docker-cleanup.service";
       };
     };
   };
